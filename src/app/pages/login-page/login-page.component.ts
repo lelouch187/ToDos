f7712093aa450a9ec54df158eb108acef25bab47
lelouch187 <vanushs187@gmail.com>
2024-06-25 17:05:05 +0600

    create login-form

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    event.preventDefault();
+    this.hide.update((value)=>!value);
+    event.stopPropagation();
+  }
 }
